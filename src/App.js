0ed24a461652e59a9b3088568b2d9f0e04a844d9
enova003 <enova003@odu.edu>
2024-07-02 22:33:51 -0500

    Basic info and colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Myers Briggs Personality Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <p>
+        Welcome to the free Myers Briggs Personality Test <br></br><br></br> Please scroll down and begin the test!
+      </p>
     </div>
   );
 }
